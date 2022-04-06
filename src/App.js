0a6a1fe347ff04ed9346e0ac0f551494e6cc5da4
LILITH-SERVER\server <lilithbe.dev@gmail.com>
2022-04-06 20:12:25 +0900

    ddd

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
-// import React, { useState } from 'react'
-// import { connect } from 'react-redux'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
 
-// import Layout from './layout/Layout'
-// import { useConfig } from './lib/useConfig'
-// import { setConfig, setLogin } from './redux'
-
-
-// const App = ({setConfig,setLogin}) => {
-//   const [isLoading, setIsLoading] = useState(false)
-//    useConfig(setIsLoading ,setLogin )
-//   return (isLoading?'loading...':<Layout/>)
-// }
-// const mapStateToProps = (state) => { 
-//   return{
-//       auth:state.auth,
-//       config:state.config,
-//   }
-// }
-// const mapDipatchToprops = (dispatch) => { 
-//    return{
-//     setLogin:(data)=>dispatch(setLogin(data)),
-//       setConfig:(data)=>dispatch(setConfig(data)),
-//    }
-// }
-// export default connect(mapStateToProps, mapDipatchToprops)(App)
+import Layout from './layout/Layout'
+import { useConfig } from './lib/useConfig'
+import { setConfig, setLogin } from './redux'
 
 
-import React from 'react'
-
-const App = () => {
-  return (
-    <h1>010-5050-2783</h1>
-  )
+const App = ({setConfig,setLogin}) => {
+  const [isLoading, setIsLoading] = useState(false)
+   useConfig(setIsLoading ,setLogin )
+  return (isLoading?'loading...':<Layout/>)
 }
-
-export default App
+const mapStateToProps = (state) => { 
+  return{
+      auth:state.auth,
+      config:state.config,
+  }
+}
+const mapDipatchToprops = (dispatch) => { 
+   return{
+    setLogin:(data)=>dispatch(setLogin(data)),
+      setConfig:(data)=>dispatch(setConfig(data)),
+   }
+}
+export default connect(mapStateToProps, mapDipatchToprops)(App)
